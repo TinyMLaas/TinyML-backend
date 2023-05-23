c8bf0d7fb072f4f3e1dc9651b8479119f9262eb5
Tuomas Marenk <tuomas.marenk@gmail.com>
2023-05-23 14:07:50 +0300

    Take new devices and add them to csv

--- routers/device.py
+++ routers/device.py
@@ -1,8 +1,24 @@
 from fastapi import APIRouter
-
+from pydantic import BaseModel, Field
 from services import device_service
 
+
+class Device(BaseModel):
+    name: str = Field(min_length=1)
+    connection: str | None = Field(default=None, min_length=1)
+    installer: str | None = Field(default=None, min_length=1)
+    compiler: str | None = Field(default=None, min_length=1)
+    model: str = Field(min_length=1)
+    description: str = Field(min_length=1)
+
+
 router = APIRouter()
+
+
+@router.post("/add_device/")
+async def add_device(device: Device):
+    device_service.add_device(device)
+
 
 @router.get("/registered_devices/")
 async def list_registered_devices():
--- services/device_service.py
+++ services/device_service.py
@@ -1,17 +1,37 @@
 import csv
 import json
- 
- 
+import pandas as pd
+from routers.device import Device
+
+
+def get_max_id():
+    df = pd.read_csv("devices.csv")
+    return df["id"].astype(int).max() + 1
+
+
+def add_device(device: Device):
+    id = get_max_id()
+    line = f"\n{id},{device.name}"
+    types = [device.connection, device.installer,
+             device.compiler, device.model, device.description]
+    for type in types:
+        if type is not None:
+            line += "," + type
+        else:
+            line += ",notgiven"
+    with open("devices.csv", "a", encoding="utf-8") as csv_file:
+        csv_file.write(line)
+
+
 def get_registered_devices():
     json_array = []
-    
+
     with open("devices.csv", "r", encoding="utf-8") as csv_file:
-            csvReader = csv.DictReader(csv_file)
-            
-            for row in csvReader:
-                json_array.append(row)
-    
-    json_string = json.dumps(json_array)     
-    
+        csvReader = csv.DictReader(csv_file)
+
+        for row in csvReader:
+            json_array.append(row)
+
+    json_string = json.dumps(json_array)
+
     return json_string
-                                
