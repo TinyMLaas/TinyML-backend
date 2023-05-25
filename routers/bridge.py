cdcfd032d1d469a38d72b531f80284d33a28d7e1
Tuomas Marenk <tuomas.marenk@gmail.com>
2023-05-25 14:32:22 +0300

    List registered bridges

--- routers/bridge.py
+++ routers/bridge.py
@@ -14,3 +14,8 @@ class Bridge(BaseModel):
 @router.post("/add_bridge/", status_code=201)
 async def add_new_bridge(bridge: Bridge):
     bridge_service.add_bridge(bridge)
+
+
+@router.get("/registered_bridges/")
+async def registered_bridges():
+    return bridge_service.get_registered_bridges()
--- services/bridge_service.py
+++ services/bridge_service.py
@@ -1,4 +1,6 @@
 import os
+import csv
+import json
 from ipaddress import IPv4Address
 from pydantic import BaseModel
 
@@ -15,3 +17,18 @@ def add_bridge(bridge: Bridge):
     row = f"\n{bridge.ip_address},{bridge.name}"
     with open(os.environ["BRIDGE_FILENAME"], 'a', encoding="utf-8") as csv:
         csv.write(row)
+
+
+def get_registered_bridges():
+    """Reads devices from a local csv file."""
+    json_array = []
+
+    with open(os.environ["BRIDGE_FILENAME"], "r", encoding="utf-8") as csv_file:
+        csvReader = csv.DictReader(csv_file)
+
+        for row in csvReader:
+            json_array.append(row)
+
+    json_string = json.dumps(json_array)
+
+    return json_string
