5719faa0a9195be567966a105c23d22fa258c1f2
shlevanto <simo.levanto@helsinki.fi>
2023-06-02 14:23:14 +0300

    Feature for datasets using database. Actual data is saved in the data/ folder of the project, database contains path to each dataset. DAtaset size is calculated in dataset_service.py when information on datasets is fetched from database.

---
+++ services/dataset_service.py
@@ -0,0 +1,40 @@
+import os
+
+from sqlalchemy.orm import Session
+from schemas import schemas
+from db import models
+
+def get_directory_size(path):
+    """Returns the size of a directory containing subdirectories in megabytes.
+    """
+
+    total_size = 0
+
+    for entry in os.scandir(path):
+        if entry.is_file():
+            total_size += entry.stat().st_size
+        else:
+            total_size += get_directory_size(entry.path)
+
+    return total_size / 1024
+
+
+def get_datasets(database: Session):
+    """Returns a list schemas.Dataset objects
+    of all available datasets in database.
+    """
+    result = database.query(models.Dataset).all()
+
+    response = []
+
+    for item in result:
+        size = f"{get_directory_size(item.path):.1f} MB"
+        dataset = schemas.Dataset(
+            id=item.id,
+            name=item.name,
+            path=item.path,
+            description=item.description,
+            size=size)
+        response.append(dataset)
+
+    return response
