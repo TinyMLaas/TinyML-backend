9a192880c9ef15692ffc8bf4a402e18b3f5ba171
Tume Marenk <95504014+T-Marenk@users.noreply.github.com>
2023-06-05 12:44:18 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -5,12 +5,8 @@ CREATE TABLE IF NOT EXISTS "Devices"(
   installer TEXT, 
   compiler TEXT, 
   model TEXT, 
-  description TEXT, 
-<<<<<<< HEAD
+  description TEXT,
   serial TEXT UNIQUE);
-=======
-  serial TEXT);
->>>>>>> 9beb2c05f36bd7ca66097c38021c086ae9484e04
 CREATE TABLE IF NOT EXISTS "Bridges"(
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   ip_address TEXT, 
