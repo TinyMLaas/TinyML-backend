391630d599af88948ff9a4bb3e2bfc730a94ee03
Tuomas Marenk <tuomas.marenk@gmail.com>
2023-06-06 13:10:35 +0300

    Image to bytes for json return

--- routers/model.py
+++ routers/model.py
@@ -1,15 +1,16 @@
 from fastapi import APIRouter, Depends
+from fastapi.responses import StreamingResponse
 from sqlalchemy.orm import Session
 from services import model_service
 from db.database import get_db
-from schemas.schemas import TrainingData, LossFunctions
+from schemas import schemas
 
 router = APIRouter()
 
 
-@router.post("/model/dataset/{dataset_id}", status_code=201)
+@router.post("/model/dataset/{dataset_id}", status_code=201, response_model=list[schemas.Models])
 async def train_model(dataset_id: int,
-                      trainingdata: TrainingData, lossfunc: LossFunctions,
+                      trainingdata: schemas.TrainingData, lossfunc: schemas.LossFunctions,
                       database: Session = Depends(get_db)):
     """return training data"""
 
