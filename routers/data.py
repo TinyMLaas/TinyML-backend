ed7ce93ca1b2b82e63c6019d6019ddfb265b7a94
shlevanto <simo.levanto@helsinki.fi>
2023-06-06 09:32:09 +0300

    Added ModelPlot subclass that includes training plot of Tensorflow model.

--- routers/data.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from services import data_service
-
-router = APIRouter()
-
-@router.get("/data/")
-async def get_dataset_names_size():
-    """Select a dataset"""
-    return data_service.get_dataset_names_size()
--- schemas/schemas.py
+++ schemas/schemas.py
@@ -102,6 +102,15 @@ class ModelCreate(ModelBase):
 
     class Config:
         orm_mode = True
+        
+
+class ModelPlot(ModelBase):
+    """"Includes image of model training plots.
+    """
+    training_plot: bytes
+    
+    class Config:
+        orm_mode = True
 
 
 # Compiled models
