a2f674450398115a219a27fb2d976aa4dddbe04e
shlevanto <simo.levanto@helsinki.fi>
2023-06-07 15:36:15 +0300

    WIP router and service for compiling. Compile service searches database for model path and dataset path.

--- routers/compiled_model.py
+++ routers/compiled_model.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 import schemas
 from services import compiled_model_service
 from db.database import get_db
-from schemas.schemas import Model, CompiledModel
+from schemas import schemas
 
 router = APIRouter()
 
@@ -12,10 +12,11 @@ router = APIRouter()
 async def compile_model(database: Session = Depends(get_db)):
     """return training data"""
 
-    response = compiled_model_service.compile()
+    response = compiled_model_service.compile_model(database, model_id=7) # dev mode
+    
     return response
 
-@router.get("/compiled_models/models/", status_code=200, response_model=list[schemas.CompiledModels])
+@router.get("/compiled_models/models/", status_code=200, response_model=list[schemas.CompiledModel])
 async def list_registered_devices(database: Session = Depends(get_db)):
     """Displays registered devices"""
     response = device_service.get_all_devices(database)
--- services/compiled_model_service.py
+++ services/compiled_model_service.py
@@ -1,8 +1,13 @@
 #dev mode
-from pathlib import Path
-import sys
-path_root = Path(__file__).parents[1]
-sys.path.append(str(path_root))
+#from pathlib import Path
+#import sys
+#path_root = Path(__file__).parents[1]
+#sys.path.append(str(path_root))
+# from fastapi import APIRouter, Depends
+# from sqlalchemy.orm import Session
+# from services import model_service
+# from db.database import get_db
+# from schemas import schemas
 
 
 import os
@@ -12,20 +17,29 @@ from services import model_service
 from db import models
 
 
-def compile_model(database: Session):
+def compile_model(database: Session, model_id: int):
+    #here we should use relationships...
     # get dataset_id based on model_id
+    model = database.query(models.Model).filter(models.Model.id == model_id).first()
+    dataset_path = model.dataset.path
+    model_path = model.model_path
+    model_params = model.parameters
+
     # get dataset.path based on dataset_id
+    # dataset = database.query(models.Dataset).filter(models.Dataset.id == dataset_id).first()
+    # dataset_path = dataset.path
+
+    # print("Dataset path for model =", dataset_path)
+
     # get model parameters: image_heigth, image_width, batch_size
+    print("Model path:", model_path)
+    compiled_model = convert_model(dataset_path=dataset_path, model_path=model_path, model_params=model_params)
 
-    dataset_path = "./data/cars_dataset"
-    model_path  ="./trained_model"
-
-    compiled_model = convert_model(dataset_path=dataset_path, model_path=model_path)
-
-from db import models
 
 def get_all_compiled_models(database: Session):
     """Returns a list of all compiled models in the database
     """
     result = database.query(models.CompiledModel).all()
     return result
+
+#compile_model(database = Session(Depends(get_db)))
