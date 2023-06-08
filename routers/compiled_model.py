4cd25f985a2170ef8a0f044dce8bcb0865d4b737
shlevanto <simo.levanto@helsinki.fi>
2023-06-08 09:29:57 +0300

    Fixed typo.

--- routers/compiled_model.py
+++ routers/compiled_model.py
@@ -8,7 +8,7 @@ from schemas import schemas
 router = APIRouter()
 
 
-@router.post("/compiled_models/models/{model_id}", status_code=201)
+@router.post("/compiled_models/models/{model_id}", status_code=201, response_model=schemas.CompiledModel)
 async def compile_model(database: Session = Depends(get_db)):
     """return training data"""
 
@@ -16,7 +16,7 @@ async def compile_model(database: Session = Depends(get_db)):
     
     return response
 
-@router.get("/compiled_models/models/", status_code=200, response_model=list[schemas.CompiledModels])
+@router.get("/compiled_models/models/", status_code=200, response_model=list[schemas.CompiledModel])
 async def get_compiled_models(database: Session = Depends(get_db)):
     """Displays registered devices"""
     response = compiled_model_service.get_all_compiled_models(database)
