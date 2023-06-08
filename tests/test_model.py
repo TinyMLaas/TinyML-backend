d6915783ce9842adb5e0e1c88e8b1596495534b8
Tuomas Marenk <tuomas.marenk@gmail.com>
2023-06-08 13:16:28 +0300

    Test new model saved, get all models from database

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,8 @@
 import sqlite3
 import os
+import shutil
 import unittest
+import json
 from fastapi.testclient import TestClient
 from main import app
 
@@ -28,8 +30,16 @@ def setup_database():
     with open("populate.sql") as f:
         populate = f.read()
 
+    model_populate = """INSERT INTO
+                      Models(created, dataset_id, parameters, description, model_path)
+                      VALUES
+                      ('2011-11-04T00:05:23Z', '2',
+                      '{"epochs": 1, "img_width": 96, "img_height": 96, "batch_size": 1}',
+                      'test_value','tensorflow_models/1')"""
+
     cur.executescript(schema)
     cur.executescript(populate)
+    cur.executescript(model_populate)
     con.commit()
     con.close()
 
@@ -37,6 +47,29 @@ def setup_database():
 def teardown_database():
     if os.path.exists("test_database.db") & cleanup:
         os.remove("test_database.db")
+    if os.path.exists("test_models/") & cleanup:
+        shutil.rmtree("test_models/")
+
+
+class GetAllModels(unittest.TestCase):
+    @classmethod
+    def setup_class(self):
+        setup_database()
+        self.client = TestClient(app)
+
+    def test_get_all_models(self):
+        response = self.client.get("/models/")
+
+        assert response.status_code == 200
+
+        res = json.loads(response.text)
+
+        self.assertEqual(1, len(res))
+        self.assertEqual(list, type(res))
+
+    @classmethod
+    def teardown_class(self):
+        teardown_database()
 
 
 class TrainNewModel(unittest.TestCase):
@@ -45,7 +78,7 @@ class TrainNewModel(unittest.TestCase):
         setup_database()
         self.client = TestClient(app)
 
-    def test_backend_returns_list_of_dataset(self):
+    def test_train_a_new_model(self):
         parameters = {
             "epochs": 1,
             "img_width": 96,
@@ -65,6 +98,16 @@ class TrainNewModel(unittest.TestCase):
 
         self.assertIsNotNone(response.text)
         assert response.status_code == 201
+
+        self.assertIn("Detect people", response.text)
+
+        res = json.loads(response.text)
+        self.assertEqual(2, res["id"])
+
+    def test_trained_model_in_directory(self):
+        dir = os.listdir("test_models/")
+
+        self.assertIn("2", dir)
 
     @classmethod
     def teardown_class(self):
